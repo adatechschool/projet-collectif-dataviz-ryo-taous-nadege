8e5998ee2fd8910ddc73d2efd505bf3d272bc955
nad_cat <130969010+npelcat@users.noreply.github.com>
2023-11-27 09:55:16 +0100

    ajout modifs du switch + re-ajout anim pluie qui avait disparue

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const APIKEY = "76d589c11f8de1e3e9b8dd346d698ce3";
 
-//function c to f 
+//function c to f
 let isCelcius = true;
 function celciusToFarenheit(celcius) {
   return (celcius * 9) / 5 + 32;
 }
-
 
 let apiCall = function (city) {
   let urlLocation = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKEY}&units=metric&lang=fr`;
@@ -18,107 +17,102 @@ let apiCall = function (city) {
           let urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=metric&lang=fr`;
 
           fetch(urlWeather).then((response) =>
-          response.json().then((data) => {
-            console.log(data);
-            document.querySelector(".city").innerHTML = data.name;
-            
-            document.querySelector(".temp").innerHTML =
-  '<i class="fa-solid fa-temperature-half"></i>' +
-  (isCelcius
-    ? data.main.temp + " °C"
-    : celciusToFarenheit(data.main.temp).toFixed(2) + " °F");
+            response.json().then((data) => {
+              console.log(data);
+              document.querySelector(".city").innerHTML = data.name;
 
+              document.querySelector(".temp").innerHTML =
+                '<i class="fa-solid fa-temperature-half"></i>' +
+                (isCelcius
+                  ? data.main.temp + " °C"
+                  : celciusToFarenheit(data.main.temp).toFixed(2) + " °F");
 
-            document.querySelector(".humidity").innerHTML =
-              '<i class="fa-solid fa-droplet"></i>' +
-              data.main.humidity +
-              " %";
-            document.querySelector(".wind").innerHTML =
-              '<i class="fa-solid fa-wind"></i>' + data.wind.speed + " km/h";
-            document.querySelector(".description").innerHTML =
-              data.weather[0].description;
+              document.querySelector(".humidity").innerHTML =
+                '<i class="fa-solid fa-droplet"></i>' +
+                data.main.humidity +
+                " %";
+              document.querySelector(".wind").innerHTML =
+                '<i class="fa-solid fa-wind"></i>' + data.wind.speed + " km/h";
+              document.querySelector(".description").innerHTML =
+                data.weather[0].description;
+
               const weather = data.weather[0].description;
-              console.log(weather);
+
               let drawElement = document.querySelector(".draw");
+              drawElement.className = "draw";
               switch (weather) {
                 case "ciel dégagé":
-                  console.log("Ciel dégagé condition reached");
                   drawElement.classList.add("soleilIcon");
                   break;
                 case "partiellement nuageux":
-                  console.log("partiellement nuageux condition reached");
                 case "peu nuageux":
-                  console.log("peu nuageux condition reached");
                   drawElement.classList.add("eclairciesIcon");
                   break;
                 case "couvert":
-                  console.log("couvert condition reached");
                 case "nuageux":
-                  console.log("nuageux condition reached");
                   drawElement.classList.add("nuageuxIcon");
                   break;
                 case "brume":
-                  console.log("brume condition reached");
                   drawElement.classList.add("brumeIcon");
                   break;
+                case "pluie":
+                case "légère pluie":
+                  drawElement.classList.add("pluieIcon");
+                  break;
                 case "orage":
-                  console.log("orage condition reached");
                   drawElement.classList.add("orageIcon");
                   break;
                 default:
                   drawElement.innerHTML = `Sorry, "${weather}" is too complicated to draw !`;
                   break;
               }
-          })
-        );
-      }
-    })
-  )
-  .catch((err) => console.log("Erreur : " + err));
+            })
+          );
+        }
+      })
+    )
+    .catch((err) => console.log("Erreur : " + err));
 };
-  document.querySelector("form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    let ville = document.getElementById("inputCity").value;
-    
-    apiCall(ville);
-  });
-    
-  document.getElementById("convert").addEventListener("click", function () {
-    isCelcius = !isCelcius
-    let ville = document.getElementById("inputCity").value;
-    apiCall(ville)
-  })
-  
-  
-  
-  apiCall("Paris");
-            
-             
-      //ANIMATION PLUIE :
-      
-      //EN DERNIER : une fois que toute la logique fonctionne, la mettre dans une fonction globale :
-      const rainMaker = () => {
-        // CREATION D'ELEMENT EN JS (injecter une balise) : createElement :
-        const rain = document.createElement("span");
-        
-        // Ajouter la classe crée en CSS pour lui donner le style voulu (add = ajouter, toggle =la mettre si elle n'y est pas, remove = retirer):
-        rain.classList.add("rain");
-        
-        //Dire au body qu'il a un nouvel enfant :
-        document.body.appendChild(rain);
-        
-        //Randomiser la taille des gouttes (entre 5 et 10px)
-        const size = Math.random() * 10 + 5 + "px";
-        rain.style.height = size;
-        rain.style.width = "1.5px";
-        
-        //Randomiser son positionnement haut-bas :
-        rain.style.top = Math.random() * 100 - 90 + "%";
-        rain.style.left = Math.random() * 100 + "%";
-        
-        //Supprimer les gouttes du DOM au bout de 3s :
-        setTimeout(() => {
-          rain.remove();
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let ville = document.getElementById("inputCity").value;
+
+  apiCall(ville);
+});
+
+document.getElementById("convert").addEventListener("click", function () {
+  isCelcius = !isCelcius;
+  let ville = document.getElementById("inputCity").value;
+  apiCall(ville);
+});
+
+apiCall("Paris");
+
+//ANIMATION PLUIE :
+
+//EN DERNIER : une fois que toute la logique fonctionne, la mettre dans une fonction globale :
+const rainMaker = () => {
+  // CREATION D'ELEMENT EN JS (injecter une balise) : createElement :
+  const rain = document.createElement("span");
+
+  // Ajouter la classe crée en CSS pour lui donner le style voulu (add = ajouter, toggle =la mettre si elle n'y est pas, remove = retirer):
+  rain.classList.add("rain");
+
+  //Dire au body qu'il a un nouvel enfant :
+  document.body.appendChild(rain);
+
+  //Randomiser la taille des gouttes (entre 5 et 10px)
+  const size = Math.random() * 10 + 5 + "px";
+  rain.style.height = size;
+  rain.style.width = "1.5px";
+
+  //Randomiser son positionnement haut-bas :
+  rain.style.top = Math.random() * 100 - 90 + "%";
+  rain.style.left = Math.random() * 100 + "%";
+
+  //Supprimer les gouttes du DOM au bout de 3s :
+  setTimeout(() => {
+    rain.remove();
   }, 3000);
 };
 
